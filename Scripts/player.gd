a7dc0fe5b70702266d60b3f6f94ae055283b4bf5
beltranbot <beltranbot@gmail.com>
2025-03-05 22:30:29 -0500

    12. game over

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -20,3 +20,10 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = -jump_force
 
 	move_and_slide()
+
+	if global_position.y > 100:
+		game_over()
+
+
+func game_over():
+	get_tree().reload_current_scene()
